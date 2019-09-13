ea856f9237053aea9dfbb4a6b39ddbdef852f5aa
RJ <RJ@RAJESH>
2019-09-13 17:14:07 +0530

    This yadav

--- src/Stringg.java
+++ src/Stringg.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 public class Stringg {
 	public static void main(String[] args) {
 	
-         String s1="chand23233232dra";
+         String s1="chand232332dra";
          int i=0;
         HashMap<Character, Integer> hashmap=new HashMap<Character, Integer>();
                  if(hashmap.containsKey(s1)){
